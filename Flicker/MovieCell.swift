ba2af1f06ca9e63bacf4f33a4adff8d419201126
umutyerli <umut@yerli.org>
2016-01-05 23:46:05 -0600

    first commit

---
+++ Flicker/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicker
+//
+//  Created by Umut Yerli on 1/5/16.
+//  Copyright © 2016 Umut Yerli. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
