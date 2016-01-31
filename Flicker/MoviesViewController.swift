3b824e71430aa66c575e5e4a417f13698d431a1f
umutyerli <umut@yerli.org>
2016-01-31 00:02:30 -0600

    2nd Commit

--- Flicker/MoviesViewController.swift
+++ Flicker/MoviesViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -44,8 +46,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 		});
 		task.resume()
 		
-		
-		
+		// Initialize a UIRefreshControl
+		let refreshControl = UIRefreshControl()
+		refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+		tableView.insertSubview(refreshControl, atIndex: 0)
 		
 		
     }
@@ -54,6 +58,60 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+	
+	func loadDataFromNetwork() {
+	
+		let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+		let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+		let request = NSURLRequest(URL: url!)
+		let session = NSURLSession(
+			configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+			delegate:nil,
+			delegateQueue:NSOperationQueue.mainQueue()
+		)
+		MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+		
+		let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+			completionHandler: { (data, response, error) in
+				
+				// Hide HUD once the network request comes back (must be done on main UI thread)
+				MBProgressHUD.hideHUDForView(self.view, animated: true)
+				
+				// ... Remainder of response handling code ...
+				
+		});
+		task.resume()
+	}
+	
+	// Makes a network request to get updated data
+	// Updates the tableView with the new data
+	// Hides the RefreshControl
+	func refreshControlAction(refreshControl: UIRefreshControl) {
+		
+		// ... Create the NSURLRequest (myRequest) ...
+		
+		let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+		let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+		let request = NSURLRequest(URL: url!)
+		let session = NSURLSession(
+			configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+			delegate:nil,
+			delegateQueue:NSOperationQueue.mainQueue()
+		)
+
+		let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+			completionHandler: { (data, response, error) in
+				
+				// ... Use the new data to update the data source ...
+				
+				// Reload the tableView now that there is new data
+				self.tableView.reloadData()
+				
+				// Tell the refreshControl to stop spinning
+				refreshControl.endRefreshing()
+		});
+		task.resume()
+	}
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		
@@ -70,12 +128,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 		let movie = movies![indexPath.row]
 		let title = movie["title"] as! String
 		let overview = movie["overview"] as! String
+		let posterPath = movie["poster_path"] as! String
+		
+		let baseUrl = "https://image.tmdb.org/t/p/w500"
+		
+		let imageUrl = NSURL(string: baseUrl + posterPath)
+		
 		
 		cell.titleLabel.text = title
 		cell.overviewLabel.text = overview
+		cell.posterView.setImageWithURL(imageUrl!)
 		
-		
-		print("row\(indexPath.row)")
 		return cell
 	}
     /*
