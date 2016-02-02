73130491ea3e601684169735a7d6482888468b36
umutyerli <umut@yerli.org>
2016-02-01 23:52:47 -0600

    setup merge

---
+++ Flicker/DetailViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DetailViewController.swift
+//  Flicker
+//
+//  Created by Umut Yerli on 2/1/16.
+//  Copyright © 2016 Umut Yerli. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+	@IBOutlet weak var posterImageView: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var overviewLabel: UILabel!
+	
+	var movie: NSDictionary!
+	
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		
+		let title = movie["title"] as? String
+		titleLabel.text = title
+		
+		let overview = movie["overview"]
+		overviewLabel.text = overview as? String
+		
+		let baseUrl = "https://image.tmdb.org/t/p/w500"
+		
+		if let posterPath = movie["poster_path"] as? String {
+			let imageUrl = NSURL(string: baseUrl + posterPath)
+			posterImageView.setImageWithURL(imageUrl!)
+		}
+		
+		
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flicker/MoviesViewController.swift
+++ Flicker/MoviesViewController.swift
@@ -128,27 +128,35 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 		let movie = movies![indexPath.row]
 		let title = movie["title"] as! String
 		let overview = movie["overview"] as! String
-		let posterPath = movie["poster_path"] as! String
+		cell.titleLabel.text = title
+		cell.overviewLabel.text = overview
 		
 		let baseUrl = "https://image.tmdb.org/t/p/w500"
 		
-		let imageUrl = NSURL(string: baseUrl + posterPath)
+		if let posterPath = movie["poster_path"] as? String {
+			let imageUrl = NSURL(string: baseUrl + posterPath)
+			cell.posterView.setImageWithURL(imageUrl!)
+		}
 		
-		
-		cell.titleLabel.text = title
-		cell.overviewLabel.text = overview
-		cell.posterView.setImageWithURL(imageUrl!)
 		
 		return cell
 	}
-    /*
+	
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		let cell = sender as! UITableViewCell
+		let indexPath = tableView.indexPathForCell(cell)
+		let movie = movies![indexPath!.row]
+		
+		let detailViewController = segue.destinationViewController as! DetailViewController
+		detailViewController.movie = movie
+		
+		print("prepare for segue")
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+	
 
 }
